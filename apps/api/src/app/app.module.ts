ad77f5eb2654f5dfcadeb74f0db6f4055ffc1805
Radnoumane MOSSABELY <mossabelyradnoumane@gmail.com>
2024-09-19 23:30:18 +0200

    refactor: reorganize imports and update database configuration in app module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,24 +3,38 @@ import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
-import {VersionsModule} from "../versions/versions.module";
-import {ServicesModule} from "../services/services.module";
-import {ArtefactsModule} from "../artefacts/artefacts.module";
-import {MigrationScriptsModule} from "../migration-scripts/migration-scripts.module";
-import {ConfigurationsModule} from "../configurations/configurations.module";
-import {DependenciesModule} from "../dependencies/dependencies.module";
-import {CredentialsModule} from "../credentials/credentials.module";
-import {EnvironmentSnapshotsModule} from "../environment-snapshots/environment-snapshots.module";
-import {EnvironmentsModule} from "../environments/environments.module";
+import {VersionsModule} from "./versions/versions.module";
+import {ServicesModule} from "./services/services.module";
+import {ArtefactsModule} from "./artefacts/artefacts.module";
+import {MigrationScriptsModule} from "./migration-scripts/migration-scripts.module";
+import {ConfigurationsModule} from "./configurations/configurations.module";
+import {DependenciesModule} from "./dependencies/dependencies.module";
+import {CredentialsModule} from "./credentials/credentials.module";
+import {EnvironmentSnapshotsModule} from "./environment-snapshots/environment-snapshots.module";
+import {EnvironmentsModule} from "./environments/environments.module";
+import { join } from 'path';
+import {Service} from "./services/entities/service.entity";
+import {Version} from "./versions/entities/version.entity";
+import {Configuration} from "./configurations/entities/configuration.entity";
+import {Artefact} from "./artefacts/entities/artefact.entity";
+import {Credential} from "./credentials/entities/credential.entity";
+import {Dependency} from "./dependencies/entities/dependency.entity";
+import {EnvironmentSnapshot} from "./environment-snapshots/entities/environment-snapshot.entity";
+import {SnapshotServiceVersion} from "./environment-snapshots/entities/snapshot-service-version.entity";
+import {Environment} from "./environments/entities/environment.entity";
+import {MigrationScript} from "./migration-scripts/entities/migration-script.entity";
+import {ServiceVersion} from "./services/entities/service-version.entity";
+import {glob} from "nx/src/generators/utils/glob";
 
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'data/database.sqlite',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Ne pas utiliser en production, synchronise entite et bdd
+      database: join(__dirname, 'data', 'database.db'),
+     entities: [Service,Version,Configuration,Artefact,Credential,Dependency,EnvironmentSnapshot,SnapshotServiceVersion,Environment,MigrationScript,ServiceVersion],
+      logging: true, // pour le débogage
+      synchronize: true, // Ne pas utiliser en production, synchronise les entites et bdd
     }),
     ServicesModule,
     VersionsModule,
@@ -29,8 +43,7 @@ import {EnvironmentsModule} from "../environments/environments.module";
     ConfigurationsModule,
     DependenciesModule,
     CredentialsModule,
-    EnvironmentsModule,
-    EnvironmentSnapshotsModule,
+    EnvironmentsModule,    EnvironmentSnapshotsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
